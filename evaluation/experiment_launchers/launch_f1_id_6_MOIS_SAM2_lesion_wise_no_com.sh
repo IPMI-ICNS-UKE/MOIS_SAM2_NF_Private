7089c8e3b03476271f6b3df88377dd328cfb584a
GeorgBell <georgbell5@gmail.com>
2025-04-13 08:09:08 +0200

    Added launchers of lesion-wise MOIS SAM no CoM and with CoM on all slices

--- evaluation/experiment_launchers/launch_f1_id_6_MOIS_SAM2_lesion_wise_no_com.sh
+++ evaluation/experiment_launchers/launch_f1_id_6_MOIS_SAM2_lesion_wise_no_com.sh
@@ -2,28 +2,30 @@
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=$(dirname "$(dirname "$(pwd)")"):$PYTHONPATH
 
-NETWORK="MOIS_SAM2"
-EVAL_MODE="lesion_wise_corrective"
 TEST_SET_IDS=(1 2 3)
 
+NETWORK="MOIS_SAM2"
+EVAL_MODE="lesion_wise_corrective"
+USE_COM_DECODER=false
+INFERENCE_ALL_SLICES=true
 
 # Please choose parameters
 FOLD=1
-NUM_INTERACTIONS_PER_LESION=1
+NUM_INTERACTIONS_PER_LESION=3
 NUM_LESIONS=20
 
-
-USE_COM_DECODER=false
-USE_ONLY_PROMPTED_EXEMPLARS=false
-INFERENCE_ALL_SLICES=true
 NUMBER_OF_EXEMPLARS=8
+USE_ONLY_PROMPTED_EXEMPLARS=false
+FILTER_EXEMPLAR_PREDICTION=true
+MIN_LESION_AREA=10
 
+RESULTS_DIR="/home/gkolokolnikov/PhD_project/nf_segmentation_interactive/NFInteractiveSegmentationBenchmarkingPrivate/evaluation/results/id_6"
 
 echo "Running $NETWORK with evaluation mode: $EVAL_MODE"
 
 for TEST_SET_ID in "${TEST_SET_IDS[@]}"; do
     echo "Processing test set $TEST_SET_ID..."
-    python ../pipelines/evaluation_pipeline.py --network_type $NETWORK --exemplar_use_com $USE_COM_DECODER --exemplar_use_only_prompted $USE_ONLY_PROMPTED_EXEMPLARS --exemplar_inference_all_slices $INFERENCE_ALL_SLICES --exemplar_num $NUMBER_OF_EXEMPLARS --fold $FOLD --evaluation_mode $EVAL_MODE --num_lesions $NUM_LESIONS --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
+    python ../pipelines/evaluation_pipeline.py --network_type $NETWORK --results_dir $RESULTS_DIR --exemplar_use_com $USE_COM_DECODER --min_lesion_area_threshold $MIN_LESION_AREA --filter_prev_prediction_components $FILTER_EXEMPLAR_PREDICTION --exemplar_use_only_prompted $USE_ONLY_PROMPTED_EXEMPLARS --exemplar_inference_all_slices $INFERENCE_ALL_SLICES --exemplar_num $NUMBER_OF_EXEMPLARS --fold $FOLD --evaluation_mode $EVAL_MODE --num_lesions $NUM_LESIONS --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
 done
 
 echo "Completed $NETWORK with $EVAL_MODE"
