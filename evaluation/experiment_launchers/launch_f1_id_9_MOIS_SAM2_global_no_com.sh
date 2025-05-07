e056653fd9e5d75c78353fec766ee4c2f7eaa237
GeorgBell <georgbell5@gmail.com>
2025-05-07 13:24:44 +0200

    Updated the scan-wise evaluation launchers

--- evaluation/experiment_launchers/launch_f1_id_9_MOIS_SAM2_global_no_com.sh
+++ evaluation/experiment_launchers/launch_f1_id_9_MOIS_SAM2_global_no_com.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=$(dirname "$(dirname "$(pwd)")"):$PYTHONPATH
 export PYTHONPATH="/home/gkolokolnikov/PhD_project/nf_segmentation_interactive/NFInteractiveSegmentationBenchmarkingPrivate/model_code/VISTA_Neurofibroma/vista3d:$PYTHONPATH"
 
@@ -19,11 +19,13 @@ USE_ONLY_PROMPTED_EXEMPLARS=false
 FILTER_EXEMPLAR_PREDICTION=true
 MIN_LESION_AREA=40
 
+CACHE_DIR="/home/gkolokolnikov/PhD_project/nf_segmentation_interactive/NFInteractiveSegmentationBenchmarkingPrivate/evaluation/cache_1"
+
 echo "Running $NETWORK with evaluation mode: $EVAL_MODE"
 
 for TEST_SET_ID in "${TEST_SET_IDS[@]}"; do
     echo "Processing test set $TEST_SET_ID..."
-    python ../pipelines/evaluation_pipeline.py --network_type $NETWORK --exemplar_use_com $USE_COM_DECODER --min_lesion_area_threshold $MIN_LESION_AREA --filter_prev_prediction_components $FILTER_EXEMPLAR_PREDICTION --exemplar_use_only_prompted $USE_ONLY_PROMPTED_EXEMPLARS --exemplar_inference_all_slices $INFERENCE_ALL_SLICES --exemplar_num $NUMBER_OF_EXEMPLARS --fold $FOLD --evaluation_mode $EVAL_MODE --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
+    python ../pipelines/evaluation_pipeline.py --cache_dir $CACHE_DIR --network_type $NETWORK --exemplar_use_com $USE_COM_DECODER --min_lesion_area_threshold $MIN_LESION_AREA --filter_prev_prediction_components $FILTER_EXEMPLAR_PREDICTION --exemplar_use_only_prompted $USE_ONLY_PROMPTED_EXEMPLARS --exemplar_inference_all_slices $INFERENCE_ALL_SLICES --exemplar_num $NUMBER_OF_EXEMPLARS --fold $FOLD --evaluation_mode $EVAL_MODE --num_interactions_per_lesion $NUM_INTERACTIONS_PER_LESION --test_set_id $TEST_SET_ID --save_predictions --use_gpu
 done
 
 echo "Completed $NETWORK with $EVAL_MODE"
