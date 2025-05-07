3b1b0e6e40b6a2009daf79c42834fdcd48f78661
GeorgBell <georgbell5@gmail.com>
2025-05-07 13:25:50 +0200

    Updated the model weight export function

--- model_weights/export_sam2_model_weights.py
+++ model_weights/export_sam2_model_weights.py
@@ -1,20 +1,20 @@
 import torch
 import os
 
-root_dir = "SAM2_finetuned"
+root_dirs = ["MOIS_SAM_aggregate_first", "MOIS_SAM_only_spatial", "MOIS_SAM_original"]
 
 # This script processes fine-tuned SAM2 model checkpoint files by extracting only the model weights.
-for fold in os.listdir(root_dir):
-    fold_path = os.path.join(root_dir, fold)
-    ckpt_path = os.path.join(fold_path, "checkpoints", "checkpoint.pt")
-    model_weights_path = os.path.join(fold_path, "checkpoint.pt")
-    
-    if os.path.isfile(ckpt_path):
-        print(f"Processing: {fold}")
+for root_dir in root_dirs:
+    for fold in os.listdir(root_dir):
+        fold_path = os.path.join(root_dir, fold)
+        ckpt_path = os.path.join(fold_path, "checkpoint", "checkpoint.pt")
+        model_weights_path = os.path.join(fold_path, "checkpoint.pt")
+        if os.path.isfile(ckpt_path):
+            print(f"Processing: {fold}")
+            
+            file_finetuned = torch.load(ckpt_path)
+            model_only_checkpoint = {'model': file_finetuned['model']}
+            torch.save(model_only_checkpoint, model_weights_path)
         
-        file_finetuned = torch.load(ckpt_path)
-        model_only_checkpoint = {'model': file_finetuned['model']}
-        torch.save(model_only_checkpoint, model_weights_path)
-    
-        print(f"Successfully converted {fold} to {model_weights_path}")  
+            print(f"Successfully converted {fold} to {model_weights_path}")  
         
